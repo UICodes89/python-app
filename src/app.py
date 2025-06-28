9cdafc0589caeb8a3a1f019e2eec7446d667500b
UICodes89 <manojkshukla87@gmail.com>
2025-06-28 15:04:31 +0100

    close text

--- src/app.py
+++ src/app.py
@@ -11,7 +11,7 @@ def info():
         'hostname': socket.gethostname(),
         'message': 'You are doing great, little human! <3',
         'deployed_on': 'kubernetes',
-        'message1git status':'you are doing grat!! Manoj Shukla!!'
+        'message1git status':'you are doing grat!! Manoj Shukla!'
     })
 
 @app.route('/api/v1/healthz')
